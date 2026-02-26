0e8fa8fc6ed6dde42c47d85d724345cdd5cd3955
AnshGajera <ansh27805@gmail.com>
2026-02-26 23:17:04 +0530

    Update resume

--- frontend/src/components/sections/hero.tsx
+++ frontend/src/components/sections/hero.tsx
@@ -83,7 +83,7 @@ const HeroSection = () => {
               <div className="mt-8 md:ml-2 flex flex-col gap-3">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1eSkVQc_f0Zmlvirdep8dz5w0QJS45fUU/view?usp=sharing"
+                    "https://drive.google.com/file/d/1BSFJfYZIGX_PaDyxzHBwsMvO4OMVTHbg/view?usp=sharing"
                   }
                   target="_blank"
                   className="flex-1"
